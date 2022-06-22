3452c2cf70ef2889c7c70fbac77b4d9097f10377
zhaobao1830 <373098777@qq.com>
2022-06-22 14:04:43 +0800

    vite.config.js文件进行配置

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  css: {
-    // 配置全局的scss文件,vite不需要安装sass-loader
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    },
+    css: {
+      // 配置全局的scss文件,vite不需要安装sass-loader
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
           @import "@/assets/scss/variable.scss";
         `
+        }
       }
+    },
+    server: {
+      open: true
     }
-  },
-  server: {
-    open: true
   }
 })
