e6f49310d2d2c4fdd3e006cc706ba0d8b11916e1
zhaobao1830 <373098777@qq.com>
2022-07-01 08:55:39 +0800

    vite.config.js文件配置别名

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -7,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
     plugins: [vue()],
     resolve: {
       alias: {
-        '@': '/src'
+        '@': resolve(__dirname, 'src')
       }
     },
     css: {
