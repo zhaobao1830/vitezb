cd3c3bf9382701dc9bf5b0408f0b43162e19061d
zhaobao1830 <373098777@qq.com>
2022-07-04 10:41:22 +0800

    修改path的引入方式

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const { resolve } = require('path')
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
