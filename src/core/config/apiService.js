59dc8d1090ab1a6861c8a440be89e7c6460309d1
zhaobao1830 <373098777@qq.com>
2022-07-04 09:27:21 +0800

    新加axios配置

---
+++ src/core/config/apiService.js
@@ -0,0 +1,6 @@
+// api管理界面
+const apiServiceConfig = {
+  getPKey: 'apiPublic/getPKey' // 获取key
+}
+
+export default apiServiceConfig
---
+++ src/core/config/index.js
@@ -0,0 +1,6 @@
+// 全局配置
+const config = {
+  baseUrl: process.env.VUE_APP_BASE_URL || '/api/'
+}
+
+export default config
