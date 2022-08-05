17b1d5bcb577fec291a745078d88945c032fe6b7
zhaobao1830 <373098777@qq.com>
2022-08-05 17:12:46 +0800

    删除调试代码

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -3,14 +3,11 @@
     <a-button @click="setUser">赋值</a-button>
 
     用户名 {{userStore.userInfo.name}}
-    <van-cell title="选择单个日期" :value="date" @click="show = true" />
-    <van-calendar v-model:show="show" @confirm="onConfirm" />
   </div>
 </template>
 
 <script setup>
   import { useUserStore } from '@/store/modules/user'
-  import { ref } from 'vue'
 
   const userStore = useUserStore()
 
@@ -21,13 +18,6 @@
     })
   }
 
-  const date = ref('')
-  const show = ref(false)
-  const onConfirm = (value) => {
-    show.value = false
-    date.value = formatDate(value)
-  }
-  const formatDate = (date) => `${date.getMonth() + 1}/${date.getDate()}`
 </script>
 
 <style scoped lang="scss">
