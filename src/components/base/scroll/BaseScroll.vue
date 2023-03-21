464003ae171795fba9ea37a86e50185255cd42b8
zhaobao1830 <373098777@qq.com>
2023-03-21 09:56:17 +0800

    新加scroll组件

---
+++ src/components/base/scroll/BaseScroll.vue
@@ -0,0 +1,39 @@
+<template>
+ <div ref="rootRef">
+   <slot></slot>
+ </div>
+</template>
+
+<script setup>
+  import useScroll from './use-scroll'
+  import { ref } from 'vue'
+
+  const props = defineProps({
+    click: {
+      type: Boolean,
+      default: true
+    },
+    probeType: {
+      type: Number,
+      default: 0
+    },
+    scrollX: {
+      type: Boolean,
+      default: false
+    }
+  })
+
+  const emits = defineEmits(['BaseScroll.vue'])
+
+  const rootRef = ref(null)
+
+  const scroll = useScroll(rootRef, props, emits)
+
+  defineExpose({
+    scroll
+  })
+</script>
+
+<style scoped lang="scss">
+
+</style>
